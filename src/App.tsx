ab037ca58311b01ab51272401764db782d4de2a7
Benjamin Gluck <benjamin.j.gluck@gmail.com>
2023-03-03 15:03:26 -0500

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import { SideNavMenu } from "./components/SideMenu";
 export default () => {
   return (
     <div className="h-screen w-screen">
-      <div className="fixed h-screen w-screen opacity-25 bg-purple-700 z-50"></div>
       <SideNavMenu></SideNavMenu>
       <div className="h-screen w-screen -z-50">
         <TopicContainer></TopicContainer>
--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState, useRef } from "react";
 import mapboxgl from "mapbox-gl";
-
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
 export const SideNavMenu = () => {
   const drawer = useRef(null);
   const [open, setOpen] = useState(true);
@@ -8,19 +8,20 @@ export const SideNavMenu = () => {
   const [lat, setLat] = useState(42.35);
 
   const onClose = () => {
-    () => setOpen(false);
+    setOpen(false);
   };
 
   const onOpen = () => {
-    if (!open) () => setOpen(true);
+    if (!open) setOpen(true);
   };
 
   return (
     <div
-      id="drawer-example"
-      className="fixed top-0 left-0 z-40 h-screen p-4 overflow-y-auto transition-transform bg-white w-64 dark:bg-gray-800"
+      className={`fixed top-0 left-0 z-40 h-screen p-4 overflow-y-auto transition bg-white w-60 dark:bg-gray-800 ${
+        !open && "-translate-x-3/4"
+      }`}
       onClick={onOpen}
-      tabIndex={-1}
+      tabIndex={0}
       ref={drawer}
       aria-labelledby="drawer-label"
     >
@@ -41,32 +42,16 @@ export const SideNavMenu = () => {
             clipRule="evenodd"
           ></path>
         </svg>
-        Gis info {open.toString()}
+        LOFTY
       </h5>
       <button
         data-drawer-hide="drawer-example"
         aria-controls="drawer-example"
         onClick={onClose}
-        className="text-gray-400 relative bg-transparent hover:bg-gray-200 hover:text-gray-900 hover:cursor-crosshair z-50 rounded-lg text-sm p-1.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+        className="text-gray-400 relative bg-transparent hover:bg-gray-200 hover:text-gray-900 float-right hover:cursor-pointer z-50 rounded-lg text-sm p-1.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
       >
-        <svg
-          // aria-hidden="true"
-          className="w-5 h-5"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fillRule="evenodd"
-            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-            clipRule="evenodd"
-          ></path>
-        </svg>
-        <span className="sr-only">Close menu</span>
+        {open ? <MdKeyboardArrowLeft size={25} /> : <MdKeyboardArrowRight size={25} />}
       </button>
     </div>
   );
 };
-function ref(arg0: null) {
-  throw new Error("Function not implemented.");
-}
