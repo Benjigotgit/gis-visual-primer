ca121076eb0a58d5c23587a25dbc93431108acad
Benjamin Gluck <benjamin.j.gluck@gmail.com>
2023-03-03 13:01:05 -0500

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { useState } from "react";
 import { MapMain } from "./components/MapMain";
 import "mapbox-gl/dist/mapbox-gl.css";
+import { TopicContainer } from "./components/TopicContainer";
+import { SideNavMenu } from "./components/SideMenu";
 
 export default () => {
   return (
-    <div className="bg-purple-600 h-screen w-screen">
-      <MapMain></MapMain>
-      <h1 className="bg-blue-500"> hi</h1>
+    <div className="h-screen w-screen">
+      <div className="fixed h-screen w-screen opacity-25 bg-purple-700 z-50"></div>
+      <SideNavMenu></SideNavMenu>
+      <div className="h-screen w-screen -z-50">
+        <TopicContainer></TopicContainer>
+
+        <MapMain></MapMain>
+      </div>
     </div>
   );
 };
---
+++ src/components/TopicContainer.tsx
@@ -0,0 +1,12 @@
+import { useEffect, useState, useRef } from "react";
+import mapboxgl from "mapbox-gl";
+
+export const TopicContainer = () => {
+  const [zoom, setZoom] = useState(9);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+
+  useEffect(() => {}, []);
+
+  return <div ref={mapContainer} className="map-container"></div>;
+};
