50fe4ff9b8c7e539b3acd86d565e19c53381aad3
Benjamin Gluck <benjamin.j.gluck@gmail.com>
2023-04-28 15:23:58 -0400

    update topic containers to have new styling

--- src/components/SideNavMenu.tsx
+++ src/components/SideNavMenu.tsx
@@ -23,41 +23,48 @@ export const SideNavMenu = () => {
 
   return (
     <div
-      className={`fixed top-0 left-0 z-50 h-screen p-4 overflow-y-auto transition duration-300 ease-out  w-60 bg-white ${
+      className={`fixed top-0 left-0 z-50 h-screen p-4 overflow-y-auto transition duration-300 ease-out drop-shadow-lg w-60 bg-white ${
         !sideNavOpen && "-translate-x-44"
       }`}
       tabIndex={0}
       ref={drawer}
       aria-labelledby="drawer-label"
     >
-      <div className="bg-[#A020F0] w-1/2 p-2 rounded h-50">
-        <img src={imageSvg} />
+      <div className="flex flex-row h-fit w-full justify-between items-center mb-4">
+        <div className="bg-[#A020F0] w-1/2 p-2 rounded h-50">
+          <img src={imageSvg} />
+        </div>
+        <button
+          data-drawer-hide="drawer-example"
+          aria-controls="drawer-example"
+          onClick={open}
+          className="text-gray-700 relative bg-transparent hover:bg-gray-200 hover:text-gray-900 float-right hover:cursor-pointer z-50 rounded-lg text-sm p-1.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+        >
+          <MdKeyboardArrowLeft
+            size={25}
+            className={`transition duration-300 ease-out ${
+              sideNavOpen ? "" : "rotate-180"
+            }`}
+          />
+        </button>
       </div>
-      <button
-        data-drawer-hide="drawer-example"
-        aria-controls="drawer-example"
-        onClick={open}
-        className="text-gray-700 relative bg-transparent hover:bg-gray-200 hover:text-gray-900 float-right hover:cursor-pointer z-50 rounded-lg text-sm p-1.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+
+      <ul
+        className={`flex flex-col justify-start w-full items-center ${
+          !sideNavOpen ? "-translate-x-24" : ""
+        }`}
       >
-        <MdKeyboardArrowLeft
-          size={25}
-          className={`transition duration-300 ease-out ${
-            sideNavOpen ? "" : "rotate-180"
-          }`}
-        />
-      </button>
-      <ul className="flex flex-col justify-start w-full items-center">
         {script.map((item, i) => {
           return (
             <li
               key={item.displayName + i}
-              className="flex flex-row justify-start items-center w-full h-10 transition hover:bg-gray-400"
+              className="flex flex-row justify-start items-center w-full h-10 transition duration-300 hover:cursor-pointer hover:bg-gray-400"
+              onClick={() => handleClickIndex(i)}
             >
               <p
                 className={` ${
                   currStepObj?.name === item?.name ? "text-[#A020F0]" : "text-slate-600"
                 }`}
-                onClick={() => handleClickIndex(i)}
               >
                 {i + 1}. {item.displayName}
               </p>
